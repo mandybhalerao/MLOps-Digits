8dba94a328764f6429971607c90fa8658ab48463
Mandar Bhalerao <mandybhalerao@Mandys-MacBook-Air.local>
2023-09-01 19:08:14 +0530

    Splitting code

--- digits.py
+++ digits.py
@@ -1,3 +1,5 @@
+# This example shows how scikit-learn can be used to recognize images of hand-written digits, from 0-9.
+
 # Author: Gael Varoquaux <gael dot varoquaux at normalesup dot org>
 # License: BSD 3 clause
 
@@ -5,35 +7,36 @@
 import matplotlib.pyplot as plt
 
 # Import datasets, classifiers and performance metrics
-from sklearn import datasets, metrics, svm
-from sklearn.model_selection import train_test_split
+from sklearn import metrics
+from utils import preprocess_data, train_model, split_data,read_digits
 
-digits = datasets.load_digits()
+# The digits dataset consists of 8x8 pixel images of digits. The images attribute of the dataset stores 8x8 arrays of grayscale values for each image. We will use these arrays to visualize the first 4 images. The target attribute of the dataset stores the digit each image represents and this is included in the title of the 4 plots below.
+# Note: if we were working from image files (e.g., ‘png’ files), we would load them using matplotlib.pyplot.imread.
 
-_, axes = plt.subplots(nrows=1, ncols=4, figsize=(10, 3))
-for ax, image, label in zip(axes, digits.images, digits.target):
-    ax.set_axis_off()
-    ax.imshow(image, cmap=plt.cm.gray_r, interpolation="nearest")
-    ax.set_title("Training: %i" % label)
 
-    # flatten the images
-n_samples = len(digits.images)
-data = digits.images.reshape((n_samples, -1))
+# 1. Data Loading
+x,y = read_digits()
 
-# Create a classifier: a support vector classifier
-clf = svm.SVC(gamma=0.001)
+# 3. Data splitting
+X_train, X_test, y_train, y_test = split_data(x, y, test_size=0.3);
 
-# Split data into 50% train and 50% test subsets
-X_train, X_test, y_train, y_test = train_test_split(
-    data, digits.target, test_size=0.5, shuffle=False
-)
+# 4. Data Preprocessing
+X_train = preprocess_data(X_train)
+X_test = preprocess_data(X_test)
 
-# Learn the digits on the train subset
-clf.fit(X_train, y_train)
+# 5. Train the data
+model = train_model(X_train, y_train, {'gamma': 0.001}, model_type='svm')
+
+
 
 # Predict the value of the digit on the test subset
-predicted = clf.predict(X_test)
+# 6. Model Predictino
+predicted = model.predict(X_test)
 
+# Below we visualize the first 4 test samples and show their predicted digit value in the title.
+
+
+# 7. Quantitative sanity check 
 _, axes = plt.subplots(nrows=1, ncols=4, figsize=(10, 3))
 for ax, image, prediction in zip(axes, X_test, predicted):
     ax.set_axis_off()
@@ -41,31 +44,18 @@ for ax, image, prediction in zip(axes, X_test, predicted):
     ax.imshow(image, cmap=plt.cm.gray_r, interpolation="nearest")
     ax.set_title(f"Prediction: {prediction}")
 
-    print(
-    f"Classification report for classifier {clf}:\n"
+# classification_report builds a text report showing the main classification metrics.
+
+#8. Model Evaluation
+print(
+    f"Classification report for classifier {model}:\n"
     f"{metrics.classification_report(y_test, predicted)}\n"
 )
+
+# We can also plot a confusion matrix of the true digit values and the predicted digit values.
 
 disp = metrics.ConfusionMatrixDisplay.from_predictions(y_test, predicted)
 disp.figure_.suptitle("Confusion Matrix")
 print(f"Confusion matrix:\n{disp.confusion_matrix}")
 
 plt.show()
-
-# The ground truth and predicted lists
-y_true = []
-y_pred = []
-cm = disp.confusion_matrix
-
-# For each cell in the confusion matrix, add the corresponding ground truths
-# and predictions to the lists
-for gt in range(len(cm)):
-    for pred in range(len(cm)):
-        y_true += [gt] * cm[gt][pred]
-        y_pred += [pred] * cm[gt][pred]
-
-print(
-    "Classification report rebuilt from confusion matrix:\n"
-    f"{metrics.classification_report(y_true, y_pred)}\n"
-)
-
