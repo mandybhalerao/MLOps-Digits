1eed3abfb2991ea24c9adfdb74889458007259a2
Mandar Bhalerao <mandybhalerao@Mandys-MacBook-Air.local>
2023-11-07 21:29:17 +0530

    Quiz3

---
+++ quiz_3/quiz_3_api.py
@@ -0,0 +1,19 @@
+from flask export Flask
+
+app = Flask(__name__)
+
+
+
+    @app.route('/model', methods=['POST'])
+def pred_model():
+    js = request.get_json()
+    image1 = js['image1']
+    image2 = js['image2']
+    #Let this be the Best Trained Model
+    model = load('./models/svm_gamma:0.001_C:0.1.joblib')
+    prediction_image_1 = model.predict(image1)
+    prediction_image_2 = model.predict(image2)
+    if(prediction_image_1 == prediction_image_2):
+        return True
+    else:
+        return False
